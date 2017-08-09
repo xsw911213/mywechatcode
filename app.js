862a13181a172917477a39efc6e6b0ab22687060
xsw911213 <410986672@qq.com>
2017-08-09 21:06:20 +0800

    '~~~'

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var Koa = require('koa')
 var sha = require('sha1')
 var config = {
 	wechat:{
-		appID:
-		appSecret:
-		token:
+		appID:"wx3228cdfc93035209",
+		appSecret:"d4624c36b6795d1d99dcf0547af5443d",
+		token:"xushaoweiwechattest"
 	}
 }
---
+++ website/app.js
@@ -0,0 +1,20 @@
+const http = require('http')
+
+const homePage = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>nodeJS</title>
+    <meta charset="UTF-8">
+  </head>
+  <body>
+    <h1>徐劭伟学node</h1>
+  </body>
+</html>
+`
+
+http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type','text/html')
+  res.end(homePage)
+}).listen(3000)
