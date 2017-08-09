3a53b7913d21f9971315373b18e69b52c4f8190e
xushaowei <xushaowei10077@autohome.com.cn>
2017-08-09 17:52:08 +0800

    readme 文件修改

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 微信学习代码
+
+安装本地代理服务
+npm install -g localtunnel
+
+开启代理端口
+lt --port 1001
---
+++ app.js
@@ -0,0 +1,9 @@
+var Koa = require('koa')
+var sha = require('sha1')
+var config = {
+	wechat:{
+		appID:
+		appSecret:
+		token:
+	}
+}
